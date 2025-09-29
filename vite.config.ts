e56047bc25fb73798f982dd2bb0ff9fd3c92596b
linzhe141 <1572213544@qq.com>
2025-09-29 13:54:58 +0800

    build: add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '.',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [react(),tailwindcss()],
+  build: {
+    minify: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes('node_modules')) return 'vendor'
+        },
+      },
+    },
+  },
+  plugins: [react(), tailwindcss()],
 })
